047bd0a4f5bb9e8a37e9c39641e0db30145fde00
José Silva <a74601@alunos.uminho.pt>
2024-02-03 23:27:57 +0000

    add gitignore

--- guessing_game/target/debug/deps/cfg_if-79793b25e6bb6564.d
+++
@@ -1,7 +0,0 @@
-C:\Users\miguel\source\repos\rust\guessing_game\target\debug\deps\libcfg_if-79793b25e6bb6564.rmeta: C:\Users\miguel\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\miguel\source\repos\rust\guessing_game\target\debug\deps\libcfg_if-79793b25e6bb6564.rlib: C:\Users\miguel\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\miguel\source\repos\rust\guessing_game\target\debug\deps\cfg_if-79793b25e6bb6564.d: C:\Users\miguel\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\miguel\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
--- guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-C:\Users\miguel\source\repos\rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-C:\Users\miguel\source\repos\rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
